a0e7488a6904193005afffa04012ff0071cb1652
Kavi Khare <kavikhare2006@gmail.com>
2026-02-24 10:29:30 +0530

    LOOPS.JS

---
+++ Loop.js
@@ -0,0 +1,13 @@
+let i;
+for(i=0; i<3; i=i+1)
+{
+    console.log(i);
+}
+let count;
+document.write("Starting Loop" + "<br/>");
+for(i=0; i<10; i++)
+{
+    document.write("Current Count :" + i);
+    document.write("<br/>");
+}
+document.write("Loop stopped!");
