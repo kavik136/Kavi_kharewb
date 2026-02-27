93eb798ccdaa5a5f76854eea17bafe04ec129be6
Kavi Khare <kavikhare2006@gmail.com>
2026-02-27 12:10:29 +0530

    Switch

---
+++ Switch.js
@@ -0,0 +1,16 @@
+let grade = "E";
+console.log("Enter switch block");
+switch(grade){
+    
+    case 'A' : console.log("Good Job");
+    break;
+    case 'B' : console.log("Pretty Good");
+    break;
+    case 'C' : console.log("Passed");
+    break;
+    case 'D' : console.log("Not so good");
+    break;
+    case 'E' : console.log("Failed")
+    break;
+    default : console.log("Unknown Grade");
+}
