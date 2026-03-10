206fc0af410a3ff71ec70c5747436d45a233f489
Kavi Khare <kavikhare2006@gmail.com>
2026-03-10 09:32:24 +0530

    CollectionJS

---
+++ Collection.js
@@ -0,0 +1,16 @@
+let collection = [
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    90009898193,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+collection.teacherName="John Mac";
+collection.phoneNo=950008193;
+
