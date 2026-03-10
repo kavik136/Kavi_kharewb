707e6c6a0916f3cb3f1887abab9119781b921003
Kavi Khare <kavikhare2006@gmail.com>
2026-03-10 09:04:16 +0530

    Function

---
+++ Button.js
@@ -0,0 +1,8 @@
+function myFunction()
+{
+    alert("Hello World");
+}
+function sayHello(name, age)
+{
+    console.log(name+" is "+ age + "years old.");
+}
---
+++ Function.js
@@ -0,0 +1,6 @@
+let res = mul(12,30);
+function mul(x,y)
+{
+    return x*y;
+}
+document.write(res);
