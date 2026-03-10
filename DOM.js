ba32a8567b770b71ec0f50cb01701f8fdfa564ec
Kavi Khare <kavikhare2006@gmail.com>
2026-03-10 10:21:49 +0530

    DOMjs

---
+++ DOM.js
@@ -0,0 +1,11 @@
+const result = document.getElementById("para");
+result.innerHTML = "my self";
+result.title="myname";
+const x = document.getElementsByClassName("p1");
+console.log(x);
+x[0].innerHTML="KAVI";
+x[1].innerHTML="KHARE";
+const result3 = document.getElementsByTagName("p");
+result3[2].innerHTML = "THIS IS A PET";
+result3[1].style.color = "BLUE";
+result3[2].innerText = "MY PARAGRAPH";
